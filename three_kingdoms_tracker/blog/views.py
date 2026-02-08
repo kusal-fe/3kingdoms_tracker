cb01d467c7bd9cc8ae69e446c621b04a3a2702ea
kusal-fe <kusalfer123@gmail.com>
2026-02-08 23:13:51 +0530

    Enable templating in blog app

--- three_kingdoms_tracker/blog/views.py
+++ three_kingdoms_tracker/blog/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author':'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'August 27 2018'
+    },
+    {
+        'author':'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'August 28 2018'
+    },
+    {
+        'author':'Joe Joestar',
+        'title': 'Blog Post 3',
+        'content': 'Third Post Content',
+        'date_posted': 'August 27 2022'
+    }
+]
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html', {'title': 'about'})
--- three_kingdoms_tracker/three_kingdoms_tracker/settings.py
+++ three_kingdoms_tracker/three_kingdoms_tracker/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
+    'event_tracker.apps.EventTrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
